3a9d0b87eb4ba8bb9366f2c16db55b49aff4a3ed
nadia indah <zudhapratama123@gmail.com>
2022-09-08 12:25:44 +0700

    solved error sj

--- resources/views/penjualan/pengirimanbarang/print_a5.blade.php
+++ resources/views/penjualan/pengirimanbarang/print_a5.blade.php
@@ -108,7 +108,7 @@
                         </b></center>
                 </td>
                 <td width="20%" style="vertical-align: top; text-align: left; font-family: sans-serif">
-                    <table border="1">
+                    <table >
                         <tr style="padding:0px;">
                             <td style="padding:0px;font-size: 70%; " colspan="3"><br />Surabaya, {{
                                 $pengirimanbarang->tanggal->format("d
