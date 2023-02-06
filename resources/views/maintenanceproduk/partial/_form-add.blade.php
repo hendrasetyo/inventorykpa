4db2cdd4a1bbf9c857322dbfaa4c2dfe06c178b2
zudhapratama21 <zudhapratama123@gmail.com>
2023-02-06 10:33:18 +0700

    fix bug ttd

--- resources/views/maintenanceproduk/partial/_form-add.blade.php
+++ resources/views/maintenanceproduk/partial/_form-add.blade.php
@@ -41,7 +41,7 @@
         <label class="col-lg-1 col-form-label text-right">Tanggal :</label>
         <div class="col-lg-4">
             <div class="input-group date">
-                <input type="text" class="form-control" name="tanggal" readonly value="{{now()->}}" id="tgl1" required/>
+                <input type="text" class="form-control" name="tanggal" readonly value="{{now()->format('d-m-Y')}}" id="tgl1" required/>
             </div>            
         </div>
         <label class="col-lg-2 col-form-label text-right">Alamat:</label>
@@ -55,8 +55,13 @@
     </div>
     
 
-    <div class="text-right mb-3">
-        <a href="javascript:caribarang()" class="btn btn-sm btn-primary"><i class="flaticon2-add"></i>Tambah Barang</a>
+    <div class="row justify-content-between">
+        <div>
+            <button type="button" class="btn btn-sm btn-danger "><i class="fas fa-volume-up"></i> Sebelum Tindakan</button>
+        </div>
+        <div class=" mb-3">
+            <a href="javascript:caribarang()" class="btn btn-sm btn-primary"><i class="flaticon2-add"></i>Tambah Barang</a>
+        </div>
     </div>
     <div class="form-group row">
         <div class="col-lg-12">
@@ -71,7 +76,7 @@
                         </tr>
                     </thead>
                     <tbody>
-
+                        
                     </tbody>
                 </table>
             </div>
@@ -79,10 +84,74 @@
 
     </div>
 
-    <div class="separator separator-dashed my-5"></div>
+    
+    <hr>
+    <div class="mt-10 form-group row">
+        <label class="col-lg-1 col-form-label text-right">Lokasi Pengerjaan</label>
+        <div class="col-lg-4">
+            <select class="form-control select2"   name="lokasi_pengerjaan" id="kt_select2_1" required>
+                <option value="Dikerjakan di Tempat">Dikerjakan di Tempat</option>
+                <option value="Dikerjakan di Workshop">Dikerjakan di Workshop</option>
+            </select>
+            @error('lokasi_pengerjaan')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror          
+        </div>
+        <label class="col-lg-1 col-form-label text-right">Tanggal Pengerjaan</label>
+        <div class="col-lg-4">
+            <div class="input-group  date">
+                <input type="date" class="form-control" name="tanggal" value="{{now()->format('d-m-Y')}}" id="tgl1" required/>
+            </div>            
+        </div>
+    </div>
+
+    <div class=" mb-10 form-group row">
+        <label class="col-lg-1 col-form-label text-right"></label>
+        <div class="col-lg-4">
+        </div>
+        <label class="col-lg-1 col-form-label text-right">Tanggal Selesai Pengerjaan</label>
+        <div class="col-lg-4">
+            <div class="input-group date">
+                <input type="date" class="form-control" name="tanggal"  value="{{now()->format('d-m-Y')}}" id="tgl1" required/>
+            </div>            
+        </div>
+    </div>
+
+    <hr>
+    <div class="row justify-content-between">
+        <div>
+            <button type="button" class="btn btn-sm btn-success"><i class="fas fa-volume-up"></i> Setelah Tindakan</button>
+        </div>
+        <div class=" mb-3">
+            <a href="javascript:caribarang()" class="btn btn-sm btn-primary"><i class="flaticon2-add"></i>Tambah Barang</a>
+        </div>
+    </div>
+    
+    <div class="form-group row">
+        <div class="col-lg-12">
+            <div id="tabel_detil" class="table-responsive">
+                <table class="table">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>Nama Sparepart</th>
+                            <th>Qty</th>
+                            <th>Pekerjaan/Penyeleseian</th>
+                            <th>Action</th>                           
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+
+    <hr>
     <div class="form-group row">
         <div class="col-lg-6">
-            <label class="">Keterangan:</label>
+            <label class="">Saran:</label>
             <div class="kt-input-icon kt-input-icon--right">
                 <textarea class="form-control" name="keterangan" id="keterangan"></textarea>
             </div>
