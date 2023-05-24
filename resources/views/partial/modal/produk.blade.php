b07c8d6614f996c5b2f27df9cf1066ddd1a1efb0
zudhapratama21 <zudhapratama123@gmail.com>
2023-05-24 14:32:46 +0700

    fix

--- resources/views/partial/modal/produk.blade.php
+++ resources/views/partial/modal/produk.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Cari Barang</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Daftar Customer</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <i aria-hidden="true" class="ki ki-close"></i>
                     </button>
