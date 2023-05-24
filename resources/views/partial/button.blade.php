9e0c15d6a76624960032493535cb7f74d0d9be05
zudhapratama21 <zudhapratama123@gmail.com>
2023-05-24 14:31:36 +0700

    fix

--- resources/views/partial/button.blade.php
+++ resources/views/partial/button.blade.php
@@ -1,7 +1,6 @@
 <div style="text-align:center;">
     <div class="d-flex flex-nowrap">
-        <a href="javascript:showCustomer({{$product_id}})" class="btn btn-outline-success  btn-hover-success btn-sm mr-3">
-                <i class="fas fa-check"></i>Cek Customer
-        </a>
+        <a href="javascript:showCustomer({{$product_id}})" class="btn btn-outline-success btn-sm  btn-hover-success btn-sm mr-3">
+                <i class="fas fa-check"></i>Cek</a>
     </div>
 </div>
