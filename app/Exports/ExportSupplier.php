4be3e1a52857b2736ed8a87268c586b4eab3503b
zudha_prtm <zudhapratama123@gmail.com>
2023-10-20 13:40:08 +0700

    last commit

--- app/Exports/ExportSupplier.php
+++ app/Exports/ExportSupplier.php
@@ -11,7 +11,47 @@ class ExportSupplier implements FromView
     
     public function view(): View
     {
-        $supplier = Supplier::with('kategori','kelurahans','kecamatans','namakota','prov')->get();
+        $supplier = Supplier::with('kategori','kelurahans','kecamatans','namakota','prov')->whereNotIn('nama',[
+        'PT. ABADINUSA USAHASEMESTA',
+        'PT. BOLD TECHNOLOGIES LEADING INDONESIA',
+        'PT. CIPTA ARKA NIAGA',
+        'PT. ENSEVAL MEDIKA PRIMA',
+        'PT. PANASONIC GOBEL LIFE SOLUTIONS MANUFACTURING INDONESIA',
+        'PT. ANUGERAH SANTOSA ABADI',
+        'PT. ANUGRAH PERSADA SEMESTA',
+        'PT. AGARINDO BIOLOGICAL LABORATORY',
+        'PT. ANUGRAH ARGON MEDICA',
+        'PT. AKURAT INTAN MADYA',
+        'PT. BIMA SATYA NUSANTARA',
+        'PT. DIAGNOSTIKA SISTIM INDONESIA',
+        'PT. DIASTIKA BIOTEKINDO',
+        'PT. ENDO INDONESIA',
+        'PT. ENSEVAL PUTERA MEGATRADING',
+        'PT. FARIZTA JAYA AGUNG',
+        'PT. GLOBAL PHARMA INDONESIA',
+        'PT. HEKSA MANUNGGAL',
+        'PT. INTISUMBER HASIL SEMPURNA',
+        'PT. INDOFA UTAMA MULTICORE',
+        'PT. KEBAYORAN PHARMA',
+        'PT. MRK DIAGNOSTICS',
+        'PT. MRK DIAGNOSTICS',
+        'CV. MITRA MEGAH MANDIRI',
+        'PT. MULTY SYNERGY PERSADA',
+        'PT. MUTUAL BAHTERA SANTOSO',
+        'PT. MERAPI UTAMA PHARMA (Sidoarjo)',
+        'PT. MERAPI UTAMA PHARMA (Sidoarjo II)',
+        'PT MERAPI UTAMA PHARMA (MALANG)',
+        'PT MERAPI UTAMA PHARMA (MALANG)',
+        'MERAPI UTAMA PHARMA',
+        'PT. MEDIAN UNITAMA',
+        'PT. NELTA MULTI GRACIA',
+        'PT. PIONER INDO NUSANTARA',
+        'PT. RAJAERBA INDOCHEM',
+        'PT. SABA INDOMEDIKA',
+        'PT. SAMUDRA CITRA PERSADA',
+        'PT. TAWADA HEALTHCARE',
+        'PT. TRANSMEDIC INDONESIA',
+        'PT. ZOE PELITA NUSANTARA',])->get();
         
         return view('laporan.master.supplier',compact('supplier'));
     }
