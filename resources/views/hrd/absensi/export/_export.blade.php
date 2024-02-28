5a041c15be61f04be948b646ae74e9aa297bdd4c
zudha_prtm <zudhapratama123@gmail.com>
2024-02-28 21:44:16 +0700

    fix

--- resources/views/hrd/absensi/export/_export.blade.php
+++ resources/views/hrd/absensi/export/_export.blade.php
@@ -5,23 +5,23 @@
                 <th colspan="6" style="background-color: turquoise;text-align: center">{{$key}}</th>
             </tr>
             <tr style="background-color: turquoise">                
-                <th style="background-color: yellow">Nama</th>
-                <th style="background-color: yellow">Tanggal</th>
-                <th style="background-color: yellow">Clock In</th>
-                <th style="background-color: yellow">Clock Out</th>
-                <th style="background-color: yellow">Work Time</th>                              
-                <th style="background-color: yellow">Status</th>  
+                <th style="background-color: yellow ; border : 1px solid black">Nama</th>
+                <th style="background-color: yellow ; border : 1px solid black">Tanggal</th>
+                <th style="background-color: yellow ; border : 1px solid black">Clock In</th>
+                <th style="background-color: yellow ; border : 1px solid black">Clock Out</th>
+                <th style="background-color: yellow ; border : 1px solid black">Work Time</th>                              
+                <th style="background-color: yellow ; border : 1px solid black">Status</th>  
             </tr>
         </thead>
         <tbody>            
             @foreach ($value as $item)
                 <tr>
-                    <td>{{ ucfirst($item['nama'])}}</td>
-                    <td>{{\Carbon\Carbon::parse($item['tanggal'])->format('d/m/Y') }}</td>
-                    <td>{{\Carbon\Carbon::parse($item['clock_in'])->format('H:i') }}</td>
-                    <td>{{\Carbon\Carbon::parse($item['clock_out'])->format('H:i') }}</td>
-                    <td>{{\Carbon\Carbon::parse($item['work_time'])->format('H:i') }}</td>
-                    <td>{{ ucfirst($item['status'])}}</td>
+                    <td style="border : 1px solid black">{{ ucfirst($item['nama'])}}</td>
+                    <td style="border : 1px solid black">{{\Carbon\Carbon::parse($item['tanggal'])->format('d/m/Y') }}</td>
+                    <td style="border : 1px solid black">{{\Carbon\Carbon::parse($item['clock_in'])->format('H:i') }}</td>
+                    <td style="border : 1px solid black">{{\Carbon\Carbon::parse($item['clock_out'])->format('H:i') }}</td>
+                    <td style="border : 1px solid black">{{\Carbon\Carbon::parse($item['work_time'])->format('H:i') }}</td>
+                    <td style="border : 1px solid black">{{ ucfirst($item['status'])}}</td>
                 </tr>
             @endforeach
             <tr></tr>
