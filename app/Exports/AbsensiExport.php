4c71c8186f305e17c75d061de489e53203658eb7
zudha_prtm <zudhapratama123@gmail.com>
2025-02-26 14:09:02 +0700

    fix

--- app/Exports/AbsensiExport.php
+++ app/Exports/AbsensiExport.php
@@ -61,7 +61,7 @@ class AbsensiExport implements FromView
 
             // dd($bulanawal);
             $tanggalawal = $tahunawal .'-'.$bulanawal.'-'.'29';
-            $tanggalakhir = $this->data['tahun'] .'-'.$this->data['bulan'].'-'.'28';
+            $tanggalakhir = $this->data['tahun'] .'-'.$this->data['bulan'].'-'.'26';
             $filteryear = $absensi;
             // $filtertanggalawal = $filteryear->where('ab.tanggal','>=',$tanggalawal);
             $filtertanggalakhir = $filteryear->whereBetween('tanggal', [$tanggalawal, $tanggalakhir]);
